00ee565b5a9951dc4e392665e6a4ec82de44d979
JacksterC <90191843+JacksterC@users.noreply.github.com>
2021-11-04 18:32:36 +0100

    Level3
    Designed Level 3

--- Assets/Animations/CameraMove.cs
+++ Assets/Animations/CameraMove.cs
@@ -6,15 +6,15 @@ public class CameraMove : MonoBehaviour
 {
 	//public GameObject MovingCamera = null;
 		
-	public float xMove = 3000;
-	public float yMove = 3000;
+	public float xMove = 200;
+	public float yMove = 200;
 	public float cameraVelocity = 0.5f;
 	
-	float x = 0;
-	float y = 0;
+	public float x = 0;
+	public float y = 0;
 	
-	int yDir = 0;
-	int xDir = 1;
+	public int yDir = 0;
+	public int xDir = 1;
 	
 	
     // Start is called before the first frame update
@@ -26,43 +26,43 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (x <= xMove && xDir == 1 && yDir == 0)
+        if (x < xMove && xDir == 1 && yDir == 0)
 		{
 			x = x + cameraVelocity;
 			transform.position = transform.position + new Vector3(cameraVelocity * Time.deltaTime, 0, 0); 
 		}
-		if (x >= xMove && xDir == 1 && yDir == 0)
+		if (!(x < xMove) && xDir == 1 && yDir == 0)
 		{
 			xDir = 0;
 			yDir = 1;
 		}
-		if (y <= yMove && xDir == 0 && yDir == 1)
+		if (y < yMove && xDir == 0 && yDir == 1)
 		{
 			y = y + cameraVelocity;
 			transform.position = transform.position + new Vector3(0, cameraVelocity * Time.deltaTime, 0); 
 		}
-		if (y >= yMove && xDir == 0 && yDir == 1)
+		if (!(y < yMove) && xDir == 0 && yDir == 1)
 		{
 			xDir = -1;
 			yDir = 0;
 		}
 		
-        if (x >= 0 && xDir == -1 && yDir == 0)
+        if (x > 0 && xDir == -1 && yDir == 0)
 		{
 			x = x - cameraVelocity;
 			transform.position = transform.position + new Vector3(-cameraVelocity * Time.deltaTime, 0, 0); 
 		}
-		if (x <= 0 && xDir == -1 && yDir == 0)
+		if (!(x > 0) && xDir == -1 && yDir == 0)
 		{
 			xDir = 0;
 			yDir = -1;
 		}
-		if (y >= 0 && xDir == 0 && yDir == -1)
+		if (y > 0 && xDir == 0 && yDir == -1)
 		{
 			y = y - cameraVelocity;
 			transform.position = transform.position + new Vector3(0, -cameraVelocity * Time.deltaTime, 0); 
 		}
-		if (y <= 0 && xDir == 0 && yDir == -1)
+		if (!(y > 0) && xDir == 0 && yDir == -1)
 		{
 			xDir = 1;
 			yDir = 0;
